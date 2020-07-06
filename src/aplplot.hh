6c73d77275cbcbb443429b9548852dcfc09f5bef
ChrisMoller <mollerware@mollerware.com>
2020-07-06 13:53:21 -0400

    stuck in all the copyright notices

--- src/aplplot.hh
+++ src/aplplot.hh
@@ -2,7 +2,7 @@
     This file is part of GNU APL, a free implementation of the
     ISO/IEC Standard 13751, "Programming Language APL, Extended"
 
-    Copyright (C) 2008-2013  Dr. Jürgen Sauermann
+    Copyright (C) 2020 Chris Moller
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
